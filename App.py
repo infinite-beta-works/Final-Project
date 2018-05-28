6cda50cd98410946232f2af596f6865bb4573791
WilliamR <nxt45@icloud.com>
2018-05-28 12:25:27 -0400

    Progress so far

--- App.py
+++ App.py
@@ -1,12 +1,35 @@
 from tkinter import *
+top = Tk()
 
-root = Tk()
-topFrame = Frame(root)
-topFrame.pack()
-bottomFrame = Frame(root)
-bottomFrame.pack(side = BOTTOM)
+top.geometry("500x500")
 
-buttom1 = Button(topFrame, text = "Button 1", fg = "red")
+months = ['Jan','Feb','Mar','April','May','June','July','Aug','Sept','Oct','Nov','Dec']
+
+variable_month = StringVar(top)
+variable_month.set(months[0])
+
+w = OptionMenu(top,variable_month,*months)
+w.pack()
+
+days = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29']
+
+variable_day = StringVar(top)
+variable_day.set(days[0])#Default Value
+
+w = OptionMenu(top,variable_day,*days)
+w.pack()
+
+def ok():
+    print("Value is: " + variable_month.get() + variable_day.get())
+    value = variable_month.get() + " " + variable_day.get()
+    label_1 = Label(top, text = value)
+    label_1.pack()    
+
+button = Button(top, text="OK", command = ok)
+button.pack()
 
 
-root.mainloop()
+
+
+
+mainloop()
